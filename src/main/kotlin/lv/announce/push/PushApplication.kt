b66ba857a8c340bda482830abbaee788550a54b7
Vladislav Yakushin <vladislavs.jakusins@citadele.lv>
2019-05-25 13:08:16 +0300

    first commit

---
+++ src/main/kotlin/lv/announce/push/PushApplication.kt
@@ -0,0 +1,11 @@
+package lv.announce.push
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PushApplication
+
+fun main(args: Array<String>) {
+	runApplication<PushApplication>(*args)
+}
